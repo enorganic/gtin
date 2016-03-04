5e5dabf0c43abb9b485635307d00dbaf7d31310f
BELAIS, DAVID I <davebelais@gmail.com>
2016-03-04 15:59:34 -0800

    Updated docs.

--- docs/conf.py
+++ docs/conf.py
@@ -14,6 +14,8 @@ copy2(
     join(d,'index.rst')
 )
 
+del d, pd
+
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.doctest',
@@ -25,20 +27,25 @@ master_doc = 'index'
 project = 'gtin'
 copyright = '2016, David Belais'
 author = 'David Belais'
+
 try:
     release = pkg_resources.get_distribution('gtin').version
 except pkg_resources.DistributionNotFound:
     release = '0.0.0'
+
 language = None
 exclude_patterns = ['_build']
 pygments_style = 'sphinx'
 todo_include_todos = False
+
 if environ.get('READTHEDOCS', None) == 'True':
     html_theme = 'default'
 else:
     html_theme = 'nature'
+
 html_static_path = ['_static']
 htmlhelp_basename = 'gtindoc'
+
 latex_documents = [
     (master_doc, 'gtin.tex', 'gtin Documentation',
      'David Belais', 'manual'),
